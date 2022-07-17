be93e9b6c0359eacbf69274b9e4b8b2f9436a4d3
Ponka Petrova <marinashell@mail.ru>
2022-07-17 22:55:11 +0300

    all done

--- init.js
+++ init.js
@@ -1,5 +1,18 @@
-
 window.onload = function()
+{
+    generationUser();
+};
+
+document.getElementById('buttonGen').addEventListener('click', function () {
+    generationUser();
+ })
+
+
+ document.getElementById('buttonClear').addEventListener('click', function () {
+    clearUser();
+ })
+
+ function generationUser()
 {
     const initPerson = personGenerator.getPerson();
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
@@ -9,5 +22,16 @@ window.onload = function()
     document.getElementById('birthYearOutput').innerText = initPerson.birthYear;
     document.getElementById('birthMpnthDayOutput').innerText = initPerson.birthMontDay;
     document.getElementById('profession').innerText = initPerson.profession;
-};
+}
 
+function clearUser()
+{
+    const initPerson = personGenerator.getPerson();
+    document.getElementById('firstNameOutput').innerText = "Иван";
+    document.getElementById('secondNameOutput').innerText = "Иванович";
+    document.getElementById('surnameOutput').innerText = "Генерация фамилии";
+    document.getElementById('genderOutput').innerText = "Генерация пола";
+    document.getElementById('birthYearOutput').innerText = "";
+    document.getElementById('birthMpnthDayOutput').innerText = "Генерация дня месяца";
+    document.getElementById('profession').innerText = "Генерация професcии";
+}
